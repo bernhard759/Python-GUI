189fa18cee3a0f019a725321e6ffba29bfa7e86e
bernhard759 <67150681+bernhard759@users.noreply.github.com>
2021-01-06 11:30:58 +0100

    Update jumble.py

--- jumble.py
+++ jumble.py
@@ -1,148 +1,149 @@
 # -*- coding: utf-8 -*-
 
 # Import
-from tkinter import *  # import tkinter
+import tkinter as tk  # import tkinter
 from random import choice, shuffle
-from tkinter import messagebox
+from tkinter import messagebox, END
 import csv
 
-# Root
-root = Tk()
-root.title("Wörter raten")
-root.iconbitmap('jumble_icon.ico')
-root.geometry("400x400")
-root.resizable(0, 0)
 
-# Background
-background_image = PhotoImage(file='img1.png') #set background image
-background_label = Label(root, image=background_image) #create background label
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+# Class
+class JumbleGUI:
 
-# Variables
-points = 0
-round = 1
-count_list = []
+    # class variables
+    round = 1
+    points = 0
+    word = ""
+    count_list = []
 
-# Function
-def shuffle_func():
+    def __init__(self):
+        # Root
+        self.root = tk.Tk()
+        self.root.title("Wörter raten")
+        self.root.iconbitmap('jumble_icon.ico')
+        self.root.geometry("400x400")
+        self.root.resizable(0, 0)
 
-    # clear answer entry box
-    entry_answer.delete(0, END)
+        # Word Label
+        self.label = tk.Label(self.root, text="", font=("Arial", 24), bg=None, height=1, width=19)
+        self.label.pack(pady=20)
 
-    # csv reader
-    with open('countries.csv', newline='', encoding="UTF-8") as f:
-        reader = csv.reader(f)
-        next(reader)
-        csv_list = list(reader)
+        # Entry box
+        self.entry_answer = tk.Entry(self.root, font=("Arial", 22))
+        self.entry_answer.pack(pady=20)
 
-    # create jumble list
-    jumble_list = []
-    for entry in csv_list:
+        # Answer Button
+        self.ans_button = tk.Button(self.root, text="Bewerten", font=("Arial", 12), command=self.answer)
+        self.ans_button.pack(pady=20)
+
+        # Score Label
+        self.score_label = tk.Label(self.root, text="Punkte: ", font=("Arial", 12))
+        self.score_label.pack(pady=20)
+
+        # Status bar
+        self.statusbar = tk.Label(self.root, text="Runde 1 von 10", width=15)
+        self.statusbar.place(rely=1.0, relx=1.0, x=0, y=0, anchor="se")
+
+        self.shuffle_func()
+
+        # Mainloop
+        self.root.mainloop()
+
+
+    # Function
+    def shuffle_func(self):
+
+        # clear answer entry box
+        self.entry_answer.delete(0, END)
+
+        # csv reader
+        with open('countries.csv', newline='', encoding="UTF-8") as f:
+            reader = csv.reader(f)
+            next(reader)
+            csv_list = list(reader)
+
+        # create jumble list
+        jumble_list = []
+        for entry in csv_list:
             jumble_list.append(entry[1])
 
-    # pick random element from list
-    global word
-    word = choice(jumble_list)
+        # pick random element from list
+        JumbleGUI.word = choice(jumble_list)
 
-    # avoid doubles
-    while word in count_list and len(count_list) < len(jumble_list):
+        # avoid doubles
+        while JumbleGUI.word in JumbleGUI.count_list and len(JumbleGUI.count_list) < len(jumble_list):
             word = choice(jumble_list)
-    count_list.append(word)
+        JumbleGUI.count_list.append(JumbleGUI.word)
 
-    # break word into letters
-    break_word = list(word)
-    print(break_word)
-    shuffle(break_word)
-    print(break_word)
+        # break word into letters
+        break_word = list(JumbleGUI.word)
+        print(break_word)
+        shuffle(break_word)
+        print(break_word)
 
-    # turn into word again
-    shuffled_word = ""
-    for letter in break_word:
-        shuffled_word += letter
-    print(shuffled_word)
+        # turn into word again
+        shuffled_word = ""
+        for letter in break_word:
+            shuffled_word += letter
+        print(shuffled_word)
 
-    # print shuffled_word to gui
-    label.config(text=shuffled_word)
+        # print shuffled_word to gui
+        self.label.config(text=shuffled_word)
 
 
-# Function
-def answer():
+    # Function
+    def answer(self):
 
-    # global variables
-    global points, round
-
-    # check if word is correct
-    if word == entry_answer.get():
-        points += 1
-        ans_label = Label(text="Richtig", fg="green", font=("Arial", 10))
-        ans_label.pack(pady=20)
-        root.after(1000, ans_label.destroy)
-    else:
-        ans_label= Label(text="Falsch", fg="red", font=("Arial", 10))
-        ans_label.pack(pady=20)
-        root.after(1000, ans_label.destroy)
-
-    # call shuffle function
-    shuffle_func()
-
-    # update score label
-    score_label.configure(text="Punkte: " + str(points))
-
-    # check if game ends
-    if round == 10:
-        # clear teh labels
-        entry_answer.delete(0, END)
-        label.config(text="")
-        # check the points and call msgbox
-        if points <=3:
-            msg = messagebox.askyesno('Spiel beendet', 'Das war keine gute Leistung!\nDeine Punktzahl beträgt: ' +
-                                      str(points) + '\nWillst du nocheinmal spielen?')
-        elif points <=7:
-            msg = messagebox.askyesno('Spiel beendet', 'Das war passabel!\nDeine Punktzahl beträgt: ' +
-                                      str(points) + '\nWillst du nocheinmal spielen?')
+        # check if word is correct
+        if JumbleGUI.word == self.entry_answer.get():
+            JumbleGUI.points += 1
+            self.ans_label = tk.Label(text="Richtig", fg="green", font=("Arial", 10))
+            self.ans_label.pack(pady=20)
+            self.root.after(1000, self.ans_label.destroy)
         else:
-            msg = messagebox.askyesno('Spiel beendet', 'Tolle Leistung!\nDeine Punktzahl beträgt: ' +
-                                      str(points) + '\nWillst du nocheinmal spielen?')
-        print(msg)
-        # handle the click
-        if msg == 1:
-            entry_answer.delete(0, END)
-            shuffle_func()
-            points = 0
-            round = 0
-            shuffle_func()
-            score_label.configure(text="Punkte: " + str(points))
-        else:
-            root.destroy()
+            self.ans_label = tk.Label(text="Falsch", fg="red", font=("Arial", 10))
+            self.ans_label.pack(pady=20)
+            self.root.after(1000, self.ans_label.destroy)
 
-    # count up round
-    if round < 10:
-        round += 1
-        statusbar.config(text="Runde {} von {}".format(round, 10))
+        # call shuffle function
+        self.shuffle_func()
+
+        # update score label
+        self.score_label.configure(text="Punkte: " + str(JumbleGUI.points))
+
+        # check if game ends
+        if JumbleGUI.round == 10:
+            # clear teh labels
+            self.entry_answer.delete(0, END)
+            self.label.config(text="")
+            # check the points and call msgbox
+            if JumbleGUI.points <= 3:
+                msg = messagebox.askyesno('Spiel beendet', 'Das war keine gute Leistung!\nDeine Punktzahl beträgt: ' +
+                                          str(JumbleGUI.points) + '\nWillst du nocheinmal spielen?')
+            elif JumbleGUI.points <= 7:
+                msg = messagebox.askyesno('Spiel beendet', 'Das war passabel!\nDeine Punktzahl beträgt: ' +
+                                          str(JumbleGUI.points) + '\nWillst du nocheinmal spielen?')
+            else:
+                msg = messagebox.askyesno('Spiel beendet', 'Tolle Leistung!\nDeine Punktzahl beträgt: ' +
+                                          str(JumbleGUI.points) + '\nWillst du nocheinmal spielen?')
+            print(msg)
+            # handle the click
+            if msg == 1:
+                self.entry_answer.delete(0, END)
+                JumbleGUI.points = 0
+                JumbleGUI.round = 0
+                self.shuffle_func()
+                self.score_label.configure(text="Punkte: " + str(JumbleGUI.points))
+            else:
+                self.root.destroy()
+
+        # count up round
+        if JumbleGUI.round < 10:
+            JumbleGUI.round += 1
+            self.statusbar.config(text="Runde {} von {}".format(JumbleGUI.round, 10))
 
 
-# Word Label
-label = Label(root, text="", font=("Arial", 24), bg=None, height=1, width=19)
-label.pack(pady=20)
+# Run
+if __name__ == "__main__":
+    Jumble = JumbleGUI()
 
-# Entry box
-entry_answer = Entry(root, font=("Arial", 22))
-entry_answer.pack(pady=20)
-
-# Answer Button
-ans_button = Button(root, text="Bewerten", font=("Arial", 12), command=answer)
-ans_button.pack(pady=20)
-
-# Score Label
-score_label = Label(root, text="Punkte: " + str(points), font=("Arial", 12))
-score_label.pack(pady=20)
-
-# Status bar
-statusbar = Label(root, text="Runde 1 von 10", width=15)
-statusbar.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
-
-# Run shuffle function
-shuffle_func()
-
-# Mainloop
-root.mainloop()
